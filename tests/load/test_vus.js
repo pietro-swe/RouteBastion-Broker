9df6e13537760bc7e6c3ceddf1ba6162fc23ed58
Pietro Piva Vieira <pietro.developer@gmail.com>
2026-01-21 21:41:28 -0300

    chore: revert changes

--- tests/load/test_vus.js
+++ tests/load/test_vus.js
@@ -2,35 +2,40 @@ import papa from "https://jslib.k6.io/papaparse/5.1.1/index.js";
 import http from "k6/http";
 import { Trend, Counter } from "k6/metrics";
 
-const durationSeconds = 2 * 60;
+const durationSeconds = 5 * 60;
 
 const metrics = {
   50: {
     duration: new Trend("http_req_duration_50"),
     requests: new Counter("http_reqs_50"),
   },
-  // 100: {
-  //   duration: new Trend("http_req_duration_100"),
-  //   requests: new Counter("http_reqs_100"),
-  // },
-  // 200: {
-  //   duration: new Trend("http_req_duration_200"),
-  //   requests: new Counter("http_reqs_200"),
-  // },
-  // 300: {
-  //   duration: new Trend("http_req_duration_300"),
-  //   requests: new Counter("http_reqs_300"),
-  // },
+  75: {
+    duration: new Trend("http_req_duration_75"),
+    requests: new Counter("http_reqs_75"),
+  },
+  100: {
+    duration: new Trend("http_req_duration_100"),
+    requests: new Counter("http_reqs_100"),
+  },
+  200: {
+    duration: new Trend("http_req_duration_200"),
+    requests: new Counter("http_reqs_200"),
+  },
+  300: {
+    duration: new Trend("http_req_duration_300"),
+    requests: new Counter("http_reqs_300"),
+  },
 };
 
 
 const userLevels = [
   50,
-  // 100,
-  // 200,
-  // 300,
+  75,
+  100,
+  200,
+  300,
 ];
-const stageDuration = "3m";
+const stageDuration = "5m";
 
 export const options = {
   scenarios: userLevels.reduce((acc, users, index) => {
@@ -38,7 +43,7 @@ export const options = {
       executor: "constant-vus",
       vus: users,
       duration: stageDuration,
-      startTime: `${index * 3}m`,
+      startTime: `${index * 5}m`,
       tags: { concurrent_users: String(users) },
     };
     return acc;
@@ -52,9 +57,10 @@ export default function () {
 
   let level;
   if (vus <= 50) level = 50;
-  // else if (vus <= 100) level = 100;
-  // else if (vus <= 200) level = 200;
-  // else level = 300;
+  else if (vus <= 75) level = 75;
+  else if (vus <= 100) level = 100;
+  else if (vus <= 200) level = 200;
+  else level = 300;
 
   metrics[level].duration.add(res.timings.duration);
   metrics[level].requests.add(1);
@@ -71,12 +77,12 @@ export function handleSummary(data) {
     if (durationMetric?.values && reqMetric?.values) {
       rows.push({
         "Concurrent Users": level,
+        "Request Count": reqMetric.values.count,
         "Requests/s": reqMetric.values.count / durationSeconds,
         "Avg Response Time (ms)": durationMetric.values.avg,
         "p90 (ms)": durationMetric.values["p(90)"],
         "p95 (ms)": durationMetric.values["p(95)"],
         "Max (ms)": durationMetric.values.max,
-        "Request Count": reqMetric.values.count,
       });
     }
   }
